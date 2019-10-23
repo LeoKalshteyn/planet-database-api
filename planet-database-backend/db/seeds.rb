f82d08c984f504d6a98ed382f346d1dfa31e4b2a
leo <leo.kalshteyn@gmail.com>
2019-10-22 20:47:04 -0700

    fixed seed data

--- planet-database-backend/db/seeds.rb
+++ planet-database-backend/db/seeds.rb
@@ -9,12 +9,12 @@
 StarSystem.destroy_all
 Planet.destroy_all
 
-s1 = StarSystem.create(name: "Proxima Centauri", distance: "4.2441", apparent_magnitude: "11.13", mass: "0.123", image: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/New_shot_of_Proxima_Centauri%2C_our_nearest_neighbour.jpg/1200px-New_shot_of_Proxima_Centauri%2C_our_nearest_neighbour.jpg"
-s2 = StarSystem.create(name: "Barnard's Star", distance: "5.958", apparent_magnitude: "9.511", mass: "0.144". image: "https://upload.wikimedia.org/wikipedia/commons/1/18/Barnardstar2006.jpg"
-s3 = StarSystem.create(name: "Epsilon Eridani", distance: "10.446", apparent_magnitude: "3.73", mass: "0.83", image: "https://www.eso.org/public/outreach/eduoff/cas/cas2002/cas-projects/sweden_eridani_1/image002.jpg"
-s4 = StarSystem.create(name: "Ross 128", distance: "11.007", apparent_magnitude: "11.1", mass: "0.168", image: "https://www.daviddarling.info/images/Ross_128.jpg"
+s1 = StarSystem.create(name: "Proxima Centauri", distance: "4.2441", apparent_magnitude: "11.13", mass: "0.123", image: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/New_shot_of_Proxima_Centauri%2C_our_nearest_neighbour.jpg/1200px-New_shot_of_Proxima_Centauri%2C_our_nearest_neighbour.jpg")
+s2 = StarSystem.create(name: "Barnard's Star", distance: "5.958", apparent_magnitude: "9.511", mass: "0.144". image: "https://upload.wikimedia.org/wikipedia/commons/1/18/Barnardstar2006.jpg")
+s3 = StarSystem.create(name: "Epsilon Eridani", distance: "10.446", apparent_magnitude: "3.73", mass: "0.83", image: "https://www.eso.org/public/outreach/eduoff/cas/cas2002/cas-projects/sweden_eridani_1/image002.jpg")
+s4 = StarSystem.create(name: "Ross 128", distance: "11.007", apparent_magnitude: "11.1", mass: "0.168", image: "https://www.daviddarling.info/images/Ross_128.jpg")
 
-p1= Planet.create(label: "Proxima b", information: "test", planet_id: 1 )
-p2= Planet.create(label: "GJ 699 b", information: "test", planet_id: 2 )
-p3= Planet.create(label: "Epsilon Eridani b", information: "test", planet_id: 3 )
-p4= Planet.create(label: "Ross 128 b", information: "test", planet_id: 4 )
+p1= Planet.create(label: "Proxima b", information: "test", star_system_id: 1 )
+p2= Planet.create(label: "GJ 699 b", information: "test", star_system_id: 2 )
+p3= Planet.create(label: "Epsilon Eridani b", information: "test", star_system_id: 3 )
+p4= Planet.create(label: "Ross 128 b", information: "test", star_system_id: 4 )
